5d275620d93432089f078cb5f047fdf5c60814b9
David Liew <61726157+david-liew@users.noreply.github.com>
2023-07-08 22:03:13 +0800

    Update bullet.dart

--- lib/game/bullet.dart
+++ lib/game/bullet.dart
@@ -3,16 +3,9 @@ import 'package:flame/components.dart';
 
 import 'enemy.dart';
 
-// This component represent a bullet in game world.
 class Bullet extends SpriteComponent with CollisionCallbacks {
-  // Speed of the bullet.
   final double _speed = 450;
-
-  // Controls the direction in which bullet travels.
   Vector2 direction = Vector2(0, -1);
-
-  // Level of this bullet. Essentially represents the
-  // level of spaceship that fired this bullet.
   final int level;
 
   Bullet({
@@ -25,9 +18,6 @@ class Bullet extends SpriteComponent with CollisionCallbacks {
   @override
   void onMount() {
     super.onMount();
-
-    // Adding a circular hitbox with radius as 0.4 times
-    //  the smallest dimension of this components size.
     final shape = CircleHitbox.relative(
       0.4,
       parentSize: size,
@@ -40,8 +30,6 @@ class Bullet extends SpriteComponent with CollisionCallbacks {
   @override
   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
     super.onCollision(intersectionPoints, other);
-
-    // If the other Collidable is Enemy, remove this bullet.
     if (other is Enemy) {
       removeFromParent();
     }
@@ -50,12 +38,7 @@ class Bullet extends SpriteComponent with CollisionCallbacks {
   @override
   void update(double dt) {
     super.update(dt);
-
-    // Moves the bullet to a new position with _speed and direction.
     position += direction * _speed * dt;
-
-    // If bullet crosses the upper boundary of screen
-    // mark it to be removed it from the game world.
     if (position.y < 0) {
       removeFromParent();
     }
